65b4c38bc5f1f5942e491d99377ff8936aaec359
dipankar-halder <dipankar15-11137@diu.edu.bd>
2024-01-12 01:27:02 +0600

    log in

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,15 +1,14 @@
-import React from "react";
+import React from 'react';
 import {
   useSignInWithEmailAndPassword,
   useSignInWithGoogle,
-} from "react-firebase-hooks/auth";
-import { useForm } from "react-hook-form";
-import { Link, useLocation, useNavigate } from "react-router-dom";
-import auth from "../../firebase.init";
+} from 'react-firebase-hooks/auth';
+import { useForm } from 'react-hook-form';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
 // import axios from "axios";
-import Loading from "../Share/Loading";
-import login from "../../Images/Login/login.jpg";
-import { toast } from "react-toastify";
+import { FaUserAlt } from 'react-icons/fa';
+import Loading from '../Share/Loading';
 
 const Login = () => {
   const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
@@ -25,7 +24,7 @@ const Login = () => {
   const navigate = useNavigate();
   const location = useLocation();
 
-  let from = location.state?.from?.pathname || "/";
+  let from = location.state?.from?.pathname || '/';
 
   if (user || gUser) {
     navigate(from, { replace: true });
@@ -42,117 +41,104 @@ const Login = () => {
       </p>
     );
   }
-  const onSubmit = async (data) => {
+  const onSubmit = async data => {
     const email = data.email;
     await signInWithEmailAndPassword(data.email, data.password);
-    toast.success("Successfully Login");
-    // const { accessToken } = await axios.post(
-    //   "https://boxberry.onrender.com/login",
-    //   {
-    //     email,
-    //   }
-    // );
-    // console.log(accessToken);
   };
 
   return (
-    <div className="flex justify-center h-screen bg-slate-700">
-      <div className="w-4/12 pt-40">
-        <img className="w-11/12 rounded-xl" src={login} alt="" />
-      </div>
-      <div className="flex justify-center items-center ">
-        <div className="card w-96 shadow-2xl bg-violet-50">
-          <div className="card-body">
-            <h2 className="text-center text-2xl">Login</h2>
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <div className="form-control w-full max-w-xs">
-                <label className="label">
-                  <span className="label-text">Email</span>
-                </label>
-                <input
-                  type="email"
-                  placeholder="Your Email"
-                  className="input input-bordered bg-white w-full max-w-xs"
-                  {...register("email", {
-                    required: {
-                      value: true,
-                      message: "Email is Required",
-                    },
-                    pattern: {
-                      value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
-                      message: "Provide a valid Email",
-                    },
-                  })}
-                />
-                <label className="label">
-                  {errors.email?.type === "required" && (
-                    <span className="label-text-alt text-red-500">
-                      {errors.email.message}
-                    </span>
-                  )}
-                  {errors.email?.type === "pattern" && (
-                    <span className="label-text-alt text-red-500">
-                      {errors.email.message}
-                    </span>
-                  )}
-                </label>
-              </div>
-              <div className="form-control w-full max-w-xs">
-                <label className="label">
-                  <span className="label-text">Password</span>
-                </label>
-                <input
-                  type="password"
-                  placeholder="Password"
-                  className="input input-bordered bg-white w-full max-w-xs"
-                  {...register("password", {
-                    required: {
-                      value: true,
-                      message: "Password is Required",
-                    },
-                    minLength: {
-                      value: 6,
-                      message: "Must be 6 characters or longer",
-                    },
-                  })}
-                />
-                <label className="label">
-                  {errors.password?.type === "required" && (
-                    <span className="label-text-alt text-red-500">
-                      {errors.password.message}
-                    </span>
-                  )}
-                  {errors.password?.type === "minLength" && (
-                    <span className="label-text-alt text-red-500">
-                      {errors.password.message}
-                    </span>
-                  )}
-                </label>
-              </div>
-
-              {signInError}
+    <div className="flex justify-center items-center h-screen">
+      <div class="card w-96 bg-slate-200 shadow-2xl ">
+        <div class="card-body items-center text-center">
+          <h2 class="card-title font-bold">
+            {' '}
+            <FaUserAlt className="text-primary" /> LogIn
+          </h2>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="form-control w-full max-w-xs">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
               <input
-                className="btn btn-orange-500 w-full text-white"
-                type="submit"
-                value="Login"
+                {...register('email', {
+                  required: {
+                    value: true,
+                    message: 'email is required',
+                  },
+                  pattern: {
+                    value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                    message: 'use a proper email',
+                  },
+                })}
+                type="email"
+                placeholder="Your email"
+                className="input input-bordered w-full max-w-xs"
               />
-            </form>
-            <p>
-              <small>
-                New to BoxBerry Motor?{" "}
-                <Link to="/createAccount" className="text-orange-600 font-bold">
-                  Create New Account
-                </Link>
-              </small>
-            </p>
-            <div className="divider">OR</div>
-            <button
-              onClick={() => signInWithGoogle()}
-              className="btn btn-outline font-black"
-            >
-              Continue With Google
-            </button>
-          </div>
+              <label className="label">
+                {errors.email?.type === 'required' && (
+                  <span className="label-text-alt text-red-500">
+                    {errors.email.message}
+                  </span>
+                )}
+                {errors.email?.type === 'pattern' && (
+                  <span className="label-text-alt text-red-500">
+                    {errors.email.message}
+                  </span>
+                )}
+              </label>
+            </div>
+            <div className="form-control w-full max-w-xs">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                {...register('password', {
+                  required: {
+                    value: true,
+                    message: 'Password is required',
+                  },
+                  minLength: {
+                    value: 6,
+                    message: 'must be 6 character or longer',
+                  },
+                })}
+                type="password"
+                placeholder="Your Password"
+                className="input input-bordered w-full max-w-xs"
+              />
+              <label className="label">
+                {errors.password?.type === 'required' && (
+                  <span className="label-text-alt text-red-500">
+                    {errors.password.message}
+                  </span>
+                )}
+                {errors.password?.type === 'minLength' && (
+                  <span className="label-text-alt text-red-500">
+                    {errors.password.message}
+                  </span>
+                )}
+              </label>
+            </div>
+            {signInError}
+            <input
+              className="btn w-full max-w-xs btn-primary "
+              type="submit"
+              value="Log in"
+            />
+          </form>
+
+          <div class="divider">OR</div>
+          <p>
+            <small>
+              New to{' '}
+              <p>
+                <small>New to Development Agriculture</small>
+              </p>
+              <Link className="btn btn-secondary " to="/createAccount">
+                Create an account
+              </Link>
+            </small>
+          </p>
         </div>
       </div>
     </div>
--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -1,27 +1,28 @@
 import { signOut } from "firebase/auth";
 import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { Link } from "react-router-dom";
-import auth from "../../../firebase.init";
-import Text from "./Text/Text";
+import { FaUser } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import auth from '../../../firebase.init';
+import Text from './Text/Text';
 
 const Home = () => {
   const [user] = useAuthState(auth);
-   const logout = () => {
-     signOut(auth);
-   };
+  const logout = () => {
+    signOut(auth);
+  };
   return (
     <div className="bg-slate-900 text-white h-screen">
-      <div className="flex justify-between items-center px-20 pt-5">
-        <Text/>
+      <div className="flex justify-between items-center px-20 pt-5 border-b-[1px] border-cyan-700">
+        <Text />
         <div>
           {user ? (
             <button className=" font-bold text-orange-500" onClick={logout}>
               Sign Out
             </button>
           ) : (
-            <Link className="text-xl" to="/login">
-              Login
+            <Link className="text-xl flex gap-2 items-center" to="/login">
+              <FaUser className="text-yellow-500" /> Login
             </Link>
           )}
         </div>
